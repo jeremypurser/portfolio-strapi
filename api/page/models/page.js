68fbde1e576851f4d01b3e5d096f99743b5ab963
Jeremy Purser <jeremypurser@gmail.com>
2020-09-22 21:40:19 -0400

    Add new line before deploy

--- api/page/models/page.js
+++ api/page/models/page.js
@@ -4,6 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+
 const deploy = require("../../../util/deploy-hook");
 
 module.exports = {
--- api/piece/models/piece.js
+++ api/piece/models/piece.js
@@ -5,6 +5,7 @@ const fetch = require("node-fetch");
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+
 const deploy = require("../../../util/deploy-hook");
 
 module.exports = {
