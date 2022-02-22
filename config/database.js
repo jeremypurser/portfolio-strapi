8bc11b212151253b79a2c436c71f6f22085fcbf7
Jeremy Purser <jeremypurser@gmail.com>
2022-02-21 21:02:02 -0500

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,24 +1,19 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
-        ssl: {
-          // rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-          rejectUnauthorized: false,
-        },
-      },
-      options: {
-        // ssl: env.bool("DATABASE_SSL", false),
-        ssl: false,
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
+    debug: false,
   },
 });
